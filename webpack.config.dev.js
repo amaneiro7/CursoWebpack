1b60bc7fe226b1dcbaef09edd520f711eeb04b9b
Andres Maneiro <amaneiro7@gmail.com>
2022-12-10 19:42:49 -0400

    preparando el projecto para la dist

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-// const { CleanWebpackPlugin} = require('clean-webpack-plugin');
 const ProgressPlugin = require('progress-webpack-plugin');
 
 module.exports = {
@@ -13,9 +10,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
-        assetModuleFilename: 'assets/images/[hash][ext][query]',
-        clean: true,
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
+    mode: 'development',
+    // watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -74,14 +72,6 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-        // new CleanWebpackPlugin(),
-        new ProgressPlugin(true),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer : [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
+        new ProgressPlugin(true),    
+    ],    
 }
